07aab54c2b1e21f7fd099cb0ee6f95011ddc8f6c
Dilip Kondaparthi <es12b1010@iith.ac.in>
2016-08-17 06:22:29 +0530

    Asked in Amazon, CarWale

--- Coding-Interview-Questions/Triplet_Sum_in_Array.cpp
+++ Coding-Interview-Questions/Triplet_Sum_in_Array.cpp
@@ -1,8 +1,8 @@
 /* Given an array A[] of n numbers and another number x, 
 Determine whether or not there exist three elements in A[] whose sum is exactly x.
 Expected time complexity is O(n^2).
-Problem Link: http://www.practice.geeksforgeeks.org/problem-page.php?pid=732
-*/
+
+Problem Link: http://www.practice.geeksforgeeks.org/problem-page.php?pid=732 */
 
 #include<iostream> 
 #include<algorithm>
